b34e6d143650d750135860d03de81852fe449680
Antti Haavikko <antti.haavikko@gmail.com>
2023-08-17 14:53:32 +0300

    Try removing dupe errors

---
+++ scripts/strip-errors.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+const chalk = require("chalk");
+
+let fileName = './dist/main.js';
+
+fs.readFile(fileName, 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  var result = data.replace(/Class extends value "\+String\(n\)\+" is not a constructor or null/g, '');
+
+  fs.writeFile(fileName, result, 'utf8', function (err) {
+     if (err) return console.log(err);
+     console.log(chalk.green(`Stripped ${10} error messages.`));
+  });
+});
