1589f5559c008f9ff55397995b1c6fb9aa18f4b7
Antti Haavikko <antti.haavikko@gmail.com>
2023-08-17 22:48:10 +0300

    Strip from html too

--- scripts/strip-errors.js
+++ scripts/strip-errors.js
@@ -1,16 +1,18 @@
 var fs = require('fs');
 const chalk = require("chalk");
 
-let fileName = './dist/main.js';
+let files = ['./dist/main.js', './dist/index.html'];
 
-fs.readFile(fileName, 'utf8', function (err, data) {
-  if (err) {
-    return console.log(err);
-  }
-  var result = data.replace(/Class extends value "\+String\(n\)\+" is not a constructor or null/g, '');
-
-  fs.writeFile(fileName, result, 'utf8', function (err) {
-     if (err) return console.log(err);
-     console.log(chalk.green(`Stripped away duplicate error messages.`));
+files.forEach(file => {
+  fs.readFile(file, 'utf8', function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    var result = data.replace(/Class extends value "\+String\(n\)\+" is not a constructor or null/g, '');
+  
+    fs.writeFile(file, result, 'utf8', function (err) {
+       if (err) return console.log(err);
+       console.log(chalk.green(`Stripped away duplicate error messages from ${file}.`));
+    });
   });
-});
+})
