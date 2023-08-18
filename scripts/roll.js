83657007c204d260163a43733d2ac6ad2664f987
Antti Haavikko <antti.haavikko@gmail.com>
2023-08-18 22:48:14 +0300

    Add roadrolling

---
+++ scripts/roll.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+const chalk = require("chalk");
+
+const html = './dist/index.html';
+const script = './dist/main.min.js';
+
+fs.readFile(html, 'utf8', function (err, htmlData) {
+    if (err) {
+        return console.log(err);
+    }
+
+    fs.readFile(script, 'utf8', function (err, jsData) {
+        if (err) {
+            return console.log(err);
+        }
+        var result = htmlData.replace(/<script>.*<\/script>/g, '<script>' + jsData + '</script>');
+
+        fs.writeFile(html, result, 'utf8', function (err) {
+            if (err) return console.log(err);
+            console.log(chalk.green(`Rolled!.`));
+        });
+    });
+});
