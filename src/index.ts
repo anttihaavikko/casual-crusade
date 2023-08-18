8b4c5afb2976c8e916aa2aa0bfe2fd8e62fd494c
Antti Haavikko <antti.haavikko@gmail.com>
2023-08-19 00:21:18 +0300

    Add back bg

--- src/index.ts
+++ src/index.ts
@@ -101,34 +101,34 @@ function tick(t: number) {
   ctx.fillStyle = "#74be75";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  // ctx.save();
-  // ctx.translate(WIDTH * 0.5, HEIGHT * 0.5);
-  // ctx.rotate(-Math.PI * 0.25);
-  // ctx.scale(1.5, 1.5);
-  // ctx.translate(-WIDTH * 0.5, -HEIGHT * 0.75);
-  // ctx.fillStyle = "#ffffff22";
-  // for(let i = 0; i < 5; i++) {
-  //   ctx.fillRect(200 * i, 0, 100, 999);
-  //   ctx.fillRect(200 * i - 70, 0, 5, 999);
-  //   ctx.fillRect(200 * i - 35, 0, 5, 999);
-  //   ctx.fillRect(200 * i + 50, 0, 5, 999);
-  //   // ctx.fillRect(0, 200 * i, 999, 100);
-  //   // ctx.fillRect(0, 200 * i - 70, 999, 5);
-  //   // ctx.fillRect(0, 200 * i - 35, 999, 5);
-  //   // ctx.fillRect(0, 200 * i + 50, 999, 5);
-  // }
-  // ctx.save();
-  // ctx.translate(WIDTH * 0.5, HEIGHT * 0.5);
-  // ctx.rotate(-Math.PI * 0.5);
-  // ctx.translate(-WIDTH * 0.5, -HEIGHT * 0.75);
-  // for(let i = 0; i < 5; i++) {
-  //   ctx.fillRect(200 * i, 0, 100, 999);
-  //   ctx.fillRect(200 * i - 70, 0, 5, 999);
-  //   ctx.fillRect(200 * i - 35, 0, 5, 999);
-  //   ctx.fillRect(200 * i + 50, 0, 5, 999);
-  // }
-  // ctx.restore();
-  // ctx.restore();
+  ctx.save();
+  ctx.translate(WIDTH * 0.5, HEIGHT * 0.5);
+  ctx.rotate(-Math.PI * 0.25);
+  ctx.scale(1.5, 1.5);
+  ctx.translate(-WIDTH * 0.5, -HEIGHT * 0.75);
+  ctx.fillStyle = "#ffffff22";
+  for(let i = 0; i < 5; i++) {
+    ctx.fillRect(200 * i, 0, 100, 999);
+    ctx.fillRect(200 * i - 70, 0, 5, 999);
+    ctx.fillRect(200 * i - 35, 0, 5, 999);
+    ctx.fillRect(200 * i + 50, 0, 5, 999);
+    // ctx.fillRect(0, 200 * i, 999, 100);
+    // ctx.fillRect(0, 200 * i - 70, 999, 5);
+    // ctx.fillRect(0, 200 * i - 35, 999, 5);
+    // ctx.fillRect(0, 200 * i + 50, 999, 5);
+  }
+  ctx.save();
+  ctx.translate(WIDTH * 0.5, HEIGHT * 0.5);
+  ctx.rotate(-Math.PI * 0.5);
+  ctx.translate(-WIDTH * 0.5, -HEIGHT * 0.75);
+  for(let i = 0; i < 5; i++) {
+    ctx.fillRect(200 * i, 0, 100, 999);
+    ctx.fillRect(200 * i - 70, 0, 5, 999);
+    ctx.fillRect(200 * i - 35, 0, 5, 999);
+    ctx.fillRect(200 * i + 50, 0, 5, 999);
+  }
+  ctx.restore();
+  ctx.restore();
 
   game.update(t, mouse);
   game.effects.update(t, mouse);
