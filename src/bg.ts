2b68d7019cbfb1881d49b7f764c97cacc2d1c2a8
Antti Haavikko <antti.haavikko@gmail.com>
2023-08-20 17:33:08 +0300

    Revert bg drawing to more dumb

--- src/bg.ts
+++ src/bg.ts
@@ -12,10 +12,21 @@ export function tartan(ctx: CanvasRenderingContext2D) {
         ctx.fillRect(200 * i - 70, 0, 5, 999);
         ctx.fillRect(200 * i - 35, 0, 5, 999);
         ctx.fillRect(200 * i + 50, 0, 5, 999);
-        ctx.fillRect(0, 200 * i, 999, 100);
-        ctx.fillRect(0, 200 * i - 70, 999, 5);
-        ctx.fillRect(0, 200 * i - 35, 999, 5);
-        ctx.fillRect(0, 200 * i + 50, 999, 5);
+        // ctx.fillRect(0, 200 * i, 999, 100);
+        // ctx.fillRect(0, 200 * i - 70, 999, 5);
+        // ctx.fillRect(0, 200 * i - 35, 999, 5);
+        // ctx.fillRect(0, 200 * i + 50, 999, 5);
     }
+    ctx.save();
+    ctx.translate(WIDTH * 0.5, HEIGHT * 0.5);
+    ctx.rotate(-Math.PI * 0.5);
+    ctx.translate(-WIDTH * 0.5, -HEIGHT * 0.75);
+    for(let i = 0; i < 5; i++) {
+    ctx.fillRect(200 * i, 0, 100, 999);
+    ctx.fillRect(200 * i - 70, 0, 5, 999);
+    ctx.fillRect(200 * i - 35, 0, 5, 999);
+    ctx.fillRect(200 * i + 50, 0, 5, 999);
+    }
+    ctx.restore();
     ctx.restore();
 }
